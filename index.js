bb9a540445179bb6a9e034582eaf9d06b60416a1
Falak Fatma <falakfatma510@gmail.com>
2025-10-03 00:44:07 +0530

    Now you can create file

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const fs = require('fs');
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.set('view engine', 'ejs')
+// app.set('views', path.join(__dirname, 'views'))
+app.get("/",(req,res)=>{
+    fs.readdir('./notes',(err,files)=>{
+        if(err){
+            console.log(err)
+        }
+    res.render("index",{files:files})  
+    console.log("files",files)
+    })
+})
+app.post("/create",(req,res)=>{ 
+    fs.writeFile(`./notes/${req.body.name.split(' ').join('')}.txt`, req.body.details,(err)=>{
+        if(err){
+            console.log(err)
+        }
+        res.redirect('/')
+    })
+    console.log(req.body)
+})
+app.listen(3000)
